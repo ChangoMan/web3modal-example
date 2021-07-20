290231ad99641f70e26b3db6dd06066742243914
Hunter Chang <changodaman@gmail.com>
2021-07-20 08:18:29 -0600

    feat: adding coinbase wallet

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,14 +2,54 @@ import WalletConnectProvider from '@walletconnect/web3-provider'
 import { providers } from 'ethers'
 import Head from 'next/head'
 import { useCallback, useEffect, useReducer } from 'react'
+import WalletLink from 'walletlink'
 import Web3Modal from 'web3modal'
 import { ellipseAddress, getChainData } from '../lib/utilities'
+
+const INFURA_ID = '460f40a260564ac4a4f4b3fffb032dad'
+
+// Coinbase walletLink init
+// let walletLink
+// let walletLinkProvider
+// if (typeof window !== 'undefined') {
+//   walletLink = new WalletLink({
+//     appName: 'coinbase',
+//   })
+
+//   // WalletLink provider
+//   walletLinkProvider = walletLink.makeWeb3Provider(
+//     `https://mainnet.infura.io/v3/${INFURA_ID}`,
+//     1
+//   )
+// }
 
 const providerOptions = {
   walletconnect: {
     package: WalletConnectProvider, // required
     options: {
-      infuraId: '460f40a260564ac4a4f4b3fffb032dad', // required
+      infuraId: INFURA_ID, // required
+    },
+  },
+  'custom-walletlink': {
+    display: {
+      logo: 'https://play-lh.googleusercontent.com/PjoJoG27miSglVBXoXrxBSLveV6e3EeBPpNY55aiUUBM9Q1RCETKCOqdOkX2ZydqVf0',
+      name: 'Coinbase',
+      description: 'Connect to Coinbase Wallet (not Coinbase App)',
+    },
+    options: {
+      appName: 'Coinbase', // Your app name
+      networkUrl: `https://mainnet.infura.io/v3/${INFURA_ID}`,
+      chainId: 1,
+    },
+    package: WalletLink,
+    connector: async (_, options) => {
+      const { appName, networkUrl, chainId } = options
+      const walletLink = new WalletLink({
+        appName,
+      })
+      const provider = walletLink.makeWeb3Provider(networkUrl, chainId)
+      await provider.enable()
+      return provider
     },
   },
 }
