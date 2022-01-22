fb6fd02929f77867b45d3e85ccd093ead3866cb1
fabianschu <48454910+fabianschu@users.noreply.github.com>
2022-01-22 13:58:30 +0100

    slimmed down header

--- components/Header.js
+++ components/Header.js
@@ -1,23 +1,8 @@
-import AppContext from '../context/AppContext'
-import { useContext } from 'react'
-
 const Header = () => {
-  const { state } = useContext(AppContext)
-
   return (
-    <div>
-      <h1>loo</h1>
-      {/* <h1 className="title">Web3Modal Example</h1>
-      {web3Provider ? (
-        <button className="button" type="button" onClick={disconnect}>
-          Disconnect
-        </button>
-      ) : (
-        <button className="button" type="button" onClick={connect}>
-          Connect
-        </button>
-      )} */}
-    </div>
+    <header>
+      <h1>Header</h1>
+    </header>
   )
 }
 
